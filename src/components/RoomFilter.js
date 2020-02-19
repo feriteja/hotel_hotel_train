fc1b5af1ce778d19cb00fc4cffe62a146be7d49b
feriteja <feriteja@gmail.com>
2020-02-19 08:38:39 +0700

    add filter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -92,6 +92,52 @@ export default function RoomFilter({ rooms }) {
           />
         </div>
         {/* end room price */}
+
+        {/* size */}
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end of size */}
+        {/* extras */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              onChange={handleChange}
+              id="breakfast"
+              checked={breakfast}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+            <input
+              type="checkbox"
+              name="pets"
+              onChange={handleChange}
+              id="pets"
+              checked={pets}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+        {/* end extras */}
       </form>
     </section>
   );
